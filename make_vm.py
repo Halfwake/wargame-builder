2858cb06b0b232ef41a4de1bd1a004ef837f0c32
Andrew <drewdudash@gmail.com>
2015-08-27 23:47:13 -0400

    Fix equality test. Python doesn't even support assignment in conditionals.

--- make_vm.py
+++ make_vm.py
@@ -15,7 +15,7 @@ hostname = sys.argv[2]
 # Create a connection to the hypervisor
 #conn = libvert.openReadOnly(None)
 conn = libvert.openReadOnly(dom)
-if conn = None:
+if conn == None:
 	print 'Failed to open connection to the hypervisor'
 	sys.exit(1)
 
